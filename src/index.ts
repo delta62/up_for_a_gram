367680c58498c341c6f2842c3c344f76a88e7516
Sam Noedel <sam.noedel@gmail.com>
2023-02-21 15:12:24 -0800

    Add bin script

--- src/index.ts
+++ src/index.ts
@@ -63,18 +63,21 @@ An unofficial CLI frontend for downforacross.com
 Key Bindings
 ------------
 
-Arrow keys:    move                            Alt-P:      Check puzzle
-Spacebar:      across/down toggle              Alt-W:      Check word
-Tab:           next clue                       Alt-C:      Check cell
-S-Tab:         previous clue                   S-Alt-P:    Reveal puzzle
-Backspace:     delete and move backward        S-Alt-W:    Reveal word
-Delete:        delete without moving           S-Alt-C:    Reveal cell
-
-Ctrl-C: quit
+Arrow keys:    Move                            Alt-P:      Check puzzle
+Spacebar:      Across/down toggle              Alt-W:      Check word
+Tab:           Next clue                       Alt-C:      Check cell
+S-Tab:         Previous clue                   S-Alt-P:    Reveal puzzle
+Backspace:     Delete and move backward        S-Alt-W:    Reveal word
+Delete:        Delete without moving           S-Alt-C:    Reveal cell
+Ctrl-C:        Quit
 
 Happy crosswording!
 `)
   process.exit()
+}
+
+if (gameId.startsWith('http')) {
+  gameId = gameId.replace(/.*\//, '')
 }
 
 main(gameId).catch(err => {
